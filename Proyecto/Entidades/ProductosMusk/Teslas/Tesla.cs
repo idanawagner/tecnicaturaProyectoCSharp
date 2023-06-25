9ea9895a722430e8bb79b64027199e09f7411579
sombraconshader <121399544+sombraconshader@users.noreply.github.com>
2023-06-25 20:50:32 -0300

    Fijense si les funca
    Mi implementación del tema de los tesla

--- Proyecto/Entidades/ProductosMusk/Teslas/Tesla.cs
+++ Proyecto/Entidades/ProductosMusk/Teslas/Tesla.cs
@@ -53,34 +53,166 @@ namespace Proyecto_2_Tema_2.Entidades.ProductosMusk.Teslas
             double serviceRealizados = Math.Floor(kilometraje / this.FrecuenciaService); // Calcular la cantidad de services realizados
 
             StringBuilder reporteService = new StringBuilder(); // Generar el reporte final de services
+            
+            int a = (int)this.FrecuenciaService;
+            int b = (int)this.FrecuenciaService;
+            int c = (int)this.FrecuenciaService;
 
             for (int i = 1; i <= serviceRealizados; i++)
             {
                 int kilometrajeService = i * (int)this.FrecuenciaService; // Kilometraje del service actual.
-
-                
+             
                 StringBuilder reporteServiceParcial = new StringBuilder(); // Reporte parcial de cada service
                 reporteServiceParcial.AppendLine($"Service {i}:");
 
-                if (kilometrajeService % 1000 == 0)
-                {
-                    reporteServiceParcial.AppendLine("Revisión de cinturones de seguridad");
-                }
+                reporteServiceParcial.AppendLine("Revision de cinturones de seguridad");
 
-                if (kilometrajeService % 2000 == 0 )
+                if (this.FrecuenciaService >= 2000)
                 {
                     reporteServiceParcial.AppendLine("Revisión de batería");
                 }
+                else if (this.FrecuenciaService == 1000)
+                {
+                    if (a == 2000)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de batería");
+                    }
+                    else if (a == 1000)
+                    {
+                        a = 2000;
+                    }
+                }
 
-                if (kilometrajeService % 2500 == 0)
+                if (this.FrecuenciaService == 3000)
                 {
                     reporteServiceParcial.AppendLine("Revisión de sistema de navegación");
                 }
+                else if (this.FrecuenciaService == 1000)
+                {
 
-                if (kilometrajeService % 3000 == 0 )
+                    if (b == 2500)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de navegación");
+                        b = 0;
+                    }
+                    else if (b == 1000)
+                    {
+                        b = 2000;
+                    }
+                    else if (b == 2000)
+                    {
+                        b = 3000;
+                    }
+                    else if (b == 3000)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de navegación");
+                        b = 500;
+                    }
+                    else if (b == 500)
+                    {
+                        b = 1500;
+                    }
+                    else if (b == 1500)
+                    {
+                        b = 2500;
+                    }
+                }
+                else if (this.FrecuenciaService == 2000)
+                {
+                    if (b == 2000)
+                    {
+                        b = 4000;
+                    }
+                    else if (b == 4000)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de navegación");
+                        b = 1500;
+                    }
+                    else if (b == 1500)
+                    {
+                        b = 3500;
+                    }
+                    else if (b == 3500)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de navegación");
+                        b = 1000;
+                    }
+                    else if (b == 1000)
+                    {
+                        b = 3000;
+                    }
+                    else if (b == 3000)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de navegación");
+                        b = 500;
+                    }
+                    else if (b == 500)
+                    {
+                        b = 2500;
+                    }
+                    else if (b == 2500)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de navegación");
+                        b = 0;
+                    }
+                    else if (b == 0)
+                    {
+                        b = 2000;
+                    }
+                }
+
+                if (this.FrecuenciaService == 3000)
                 {
                     reporteServiceParcial.AppendLine("Revisión de sistema de tracción");
                     reporteServiceParcial.AppendLine("Revisión de motor");
+                }
+                else if (this.FrecuenciaService == 2000)
+                {
+                    if (c == 3000)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de tracción");
+                        reporteServiceParcial.AppendLine("Revisión de motor");
+                        c = 0;
+                    }
+                    else if (c == 0)
+                    {
+                        c = 2000;
+                    }
+                    else if (c == 2000)
+                    {
+                        c = 4000;
+                    }
+                    else if (c == 4000)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de tracción");
+                        reporteServiceParcial.AppendLine("Revisión de motor");
+                        c = 1000;
+                    }
+                    else if (c == 1000)
+                    {
+                        c = 3000;
+                    }
+                }
+                else if (this.FrecuenciaService == 1000)
+                {
+                    if (c == 3000)
+                    {
+                        reporteServiceParcial.AppendLine("Revisión de sistema de tracción");
+                        reporteServiceParcial.AppendLine("Revisión de motor");
+                        c = 0;
+                    }
+                    else if (c == 2000)
+                    {
+                        c = 3000;
+                    }
+                    else if (c == 1000)
+                    {
+                        c = 2000;
+                    }
+                    else if (c == 0)
+                    {
+                        c = 1000;
+                    }
                 }
 
                 reporteService.AppendLine(reporteServiceParcial.ToString());
